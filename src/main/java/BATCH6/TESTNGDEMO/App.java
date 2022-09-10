8a8ba2138a9bc03485b186943d1673685854f75e
GADDAM TEJESWANI <tejagaddam2000@gmail.com>
2022-09-10 17:00:44 +0530

    first commit

---
+++ src/main/java/BATCH6/TESTNGDEMO/App.java
@@ -0,0 +1,13 @@
+package BATCH6.TESTNGDEMO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
